66325fd18be205a436ce297222ee12d34f035319
Carina Kan <carina.d.kan@gmail.com>
2020-01-07 15:59:05 -0800

    Add signup/login forms

---
+++ frontend/components/app.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Provider } from 'react-redux';
+import {
+    Route,
+    Redirect,
+    Switch,
+    Link,
+    HashRouter
+} from 'react-router-dom';
+
+import GreetingContainer from './greeting/greeting_container';
+import SignupFormContainer from './session_form/signup_form_container';
+import LoginFormContainer from './session_form/login_form_container';
+
+const App = () => (
+    <div>
+        <header>
+            <Link to="/" className="header-link">
+                <h1>Curator</h1>
+            </Link>
+            <GreetingContainer />
+        </header>
+
+        <Route path="/login" component={LoginFormContainer} />
+        <Route path="/signup" component={SignupFormContainer} />
+    </div>
+);
+
+export default App;
+
--- frontend/curator.jsx
+++ frontend/curator.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// import {postUser, postSession, deleteSession} from './util/session_api_util';
 import configureStore from "./store/store";
+import Root from './components/root';
 
 document.addEventListener("DOMContentLoaded", () => {
-    const root = document.getElementById("root");
     const store = configureStore();
+
     window.getState = store.getState;
     window.dispatch = store.dispatch;
-    ReactDOM.render(<h1>Welcome to Curator</h1>, root);
+
+    const root = document.getElementById("root");
+    ReactDOM.render(<Root store={store} />, root);
 });
